4d352af620a723519f713c4831aac73bf262bef2
jatin31195 <jatinsingh31185@gmail.com>
2025-10-18 23:21:24 +0530

    updated AuthContext for complete user data

--- Server/src/utils/me.js
+++ Server/src/utils/me.js
@@ -1,13 +1,18 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js"; 
 
-export const getMe = (req, res) => {
+export const getMe = async (req, res) => {
   try {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    res.status(200).json({ userId: decoded.userId });
+    const user = await User.findById(decoded.userId).select("-password"); 
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ user });
   } catch (err) {
+    console.error(err);
     res.status(401).json({ message: "Invalid or expired token" });
   }
 };
