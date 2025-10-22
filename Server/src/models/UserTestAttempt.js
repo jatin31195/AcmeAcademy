2f2f55c323e93f8d94bdca1bd80baa361b6e2024
jatin31195 <jatinsingh31185@gmail.com>
2025-10-22 11:58:29 +0530

    Added timetaken for each question and complete test

--- Server/src/models/UserTestAttempt.js
+++ Server/src/models/UserTestAttempt.js
@@ -4,12 +4,14 @@ const answerSchema = new mongoose.Schema({
   question: { type: mongoose.Schema.Types.ObjectId, required: true },
   answer: { type: String },
   marksObtained: { type: Number, default: 0 },
+  timeTaken: { type: Number, default: 0 },
 });
 
 const userTestAttemptSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   test: { type: mongoose.Schema.Types.ObjectId, ref: "Test", required: true },
   answers: [answerSchema],
+  totalTimeTaken: { type: Number, default: 0 },
   score: { type: Number, default: 0 },
   submittedAt: { type: Date, default: Date.now },
   attemptNumber: { type: Number, required: true },
