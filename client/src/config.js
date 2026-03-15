f03b403b98c6cfa3e98f8df418a03a9fe4a9251a
jatin9125 <jatin@xoidlabs.com>
2026-03-15 12:24:24 +0530

    update env import type

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-export const BASE_URL = process.env.ACME_NODEJS_BACKEND || 'http://localhost:5000';
-export const Flask_URL= process.env.ACME_FLASK_BACKEND || 'http://localhost:5001';
+export const BASE_URL =
+  import.meta.env.VITE_NODEJS_BACKEND || "http://localhost:5000";
+
+export const Flask_URL =
+  import.meta.env.VITE_FLASK_BACKEND || "http://localhost:5001";
