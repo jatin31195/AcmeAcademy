486b328e625ac28d5cced32319f8368cb731863c
jatin9125 <jatin@xoidlabs.com>
2026-03-15 11:45:10 +0530

    updated config for backend route

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = process.env.ACME_NODEJS_BACKEND || 'http://localhost:5000';
+export const Flask_URL= process.env.ACME_FLASK_BACKEND || 'http://localhost:5001';
